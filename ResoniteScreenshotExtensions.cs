dfe0df49ea023ffe12a193ec7cbe1f1e9c929d70
hantabaru1014 <hantabaru1014@gmail.com>
2024-12-29 22:34:19 +0900

    update version

--- ResoniteScreenshotExtensions.cs
+++ ResoniteScreenshotExtensions.cs
@@ -7,7 +7,7 @@ public partial class ResoniteScreenshotExtensions : ResoniteMod
 {
     public override string Name => "ResoniteScreenshotExtensions";
     public override string Author => "hantabaru1014";
-    public override string Version => "1.1.4";
+    public override string Version => "1.1.5";
     public override string Link => "https://github.com/hantabaru1014/ResoniteScreenshotExtensions";
 
     [AutoRegisterConfigKey]
