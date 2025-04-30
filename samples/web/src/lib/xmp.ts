5f753c1225b920be5ce761628ec21bad9ef2cdd6
hantabaru1014 <hantabaru1014@gmail.com>
2025-05-01 00:11:28 +0900

    feat: (web) parse v1 metadata

--- samples/web/src/lib/xmp.ts
+++ samples/web/src/lib/xmp.ts
@@ -1,3 +1,5 @@
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+
 import { XMLParser } from "fast-xml-parser";
 import {
   float3,
@@ -27,37 +29,53 @@ export const loadXMP = async (file: File) => {
   return unescapeUnicode(match[0]);
 };
 
-export const parseMetadata = (xml: string) => {
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: "",
-    isArray: (tagName) => ["rdf:RDF", "rse:UserInfos"].includes(tagName),
-    parseAttributeValue: true,
-  });
-  const xmpData = parser.parse(xml);
-  if (!xmpData?.["rdf:RDF"]) {
-    throw new Error("Invalid XMP format");
+const parseComponentJson = (json: string) => {
+  const obj = JSON.parse(json);
+  const component = obj["Component"];
+  if (!component) {
+    throw new Error("Invalid JSON format");
   }
-  const modDescription = Array.isArray(xmpData["rdf:RDF"])
-    ? xmpData["rdf:RDF"]
-        .filter(
-          (item) =>
-            item["rdf:Description"]?.["xmlns:rse"] != null ||
-            item["rdf:Description"]?.["xmlns:resonite-ss-ext"] != null,
-        )
-        .map((item) => item["rdf:Description"])
-    : [];
-  if (modDescription.length !== 1) {
-    throw new Error("Invalid XMP format");
-  }
-  const obj = modDescription[0];
-  if (obj["xmlns:rse"] == null && obj["xmlns:resonite-ss-ext"] != null) {
-    throw new Error(
-      "Screenshots taken with this mod version are not supported. Please use version 2.0 or later.",
-    );
-  }
+  const metadata: Metadata = {
+    locationName: component["LocationName"]["Data"],
+    locationURL: component["LocationURL"]["Data"],
+    locationHost: {
+      id: component["LocationHost"]["_userId"]["Data"],
+      machineId: component["LocationHost"]["_machineId"]["Data"],
+    },
+    locationAccessLevel: component["LocationAccessLevel"]["Data"],
+    locationHiddenFromListing: component["LocationHiddenFromListing"]["Data"],
+    timeTaken: new Date(component["TimeTaken"]["Data"]),
+    takenBy: {
+      id: component["TakenBy"]["_userId"]["Data"],
+      machineId: component["TakenBy"]["_machineId"]["Data"],
+    },
+    takenGlobalPosition: component["TakenGlobalPosition"]["Data"],
+    takenGlobalRotation: component["TakenGlobalRotation"]["Data"],
+    takenGlobalScale: component["TakenGlobalScale"]["Data"],
+    appVersion: component["AppVersion"]["Data"],
+    userInfos: component["UserInfos"]["Data"].map((userInfo: any) => ({
+      user: {
+        id: userInfo["User"]["_userId"]["Data"],
+        machineId: userInfo["User"]["_machineId"]["Data"],
+      },
+      isInVR: userInfo["IsInVR"]["Data"],
+      isPresent: userInfo["IsPresent"]["Data"],
+      headPosition: userInfo["HeadPosition"]["Data"],
+      headOrientation: userInfo["HeadOrientation"]["Data"],
+      sessionJoinTimestamp: new Date(userInfo["SessionJoinTimestamp"]["Data"]),
+    })),
+    cameraManufacturer: component["CameraManufacturer"]["Data"],
+    cameraModel: component["CameraModel"]["Data"],
+    cameraFOV: component["CameraFOV"]["Data"],
+    is360: component["Is360"]["Data"],
+    stereoLayout: component["StereoLayout"]["Data"],
+  };
 
-  const parseUser = (obj: Record<string, unknown>) => {
+  return metadata;
+};
+
+const parseXML = (xmlObj: any) => {
+  const parseUser = (obj: any) => {
     const prefix = "rse:U-";
     return Object.fromEntries(
       Object.entries(obj)
@@ -85,7 +103,7 @@ export const parseMetadata = (xml: string) => {
       Number,
     ) as float3;
   };
-  const parseUserInfo = (obj: Record<string, unknown>) => {
+  const parseUserInfo = (obj: any) => {
     const user = parseUser(obj);
     const prefix = "rse:UI-";
     const info = Object.fromEntries(
@@ -113,7 +131,7 @@ export const parseMetadata = (xml: string) => {
 
     return info as MetadataUserInfo;
   };
-  const parseMetadata = (obj: Record<string, unknown>) => {
+  const parseMetadata = (obj: any) => {
     const prefix = "rse:";
     return Object.fromEntries(
       Object.entries(obj)
@@ -136,15 +154,13 @@ export const parseMetadata = (xml: string) => {
             newValue = new Date(e[1] as string);
           }
           if (newKey === "locationHost") {
-            newValue = parseUser(e[1] as Record<string, unknown>);
+            newValue = parseUser(e[1]);
           }
           if (newKey === "takenBy") {
-            newValue = parseUser(e[1] as Record<string, unknown>);
+            newValue = parseUser(e[1]);
           }
-          if (newKey === "userInfos") {
-            newValue = (e[1] as Record<string, unknown>[]).map((o) =>
-              parseUserInfo(o["rse:UserInfo"] as Record<string, unknown>),
-            );
+          if (newKey === "userInfos" && Array.isArray(e[1])) {
+            newValue = e[1].map((o: any) => parseUserInfo(o["rse:UserInfo"]));
           }
           if (newKey === "locationAccessLevel") {
             newValue = e[1] as keyof typeof SessionAccessLevel;
@@ -157,9 +173,42 @@ export const parseMetadata = (xml: string) => {
         }),
     ) as Metadata;
   };
-  const metadata = parseMetadata(obj);
 
-  return metadata;
+  return parseMetadata(xmlObj);
+};
+
+export const parseMetadata = (xml: string) => {
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: "",
+    isArray: (tagName) => ["rdf:RDF", "rse:UserInfos"].includes(tagName),
+    parseAttributeValue: true,
+  });
+  const xmpData = parser.parse(xml);
+  if (!xmpData?.["rdf:RDF"]) {
+    throw new Error("XMP data not found");
+  }
+  const modDescription = Array.isArray(xmpData["rdf:RDF"])
+    ? xmpData["rdf:RDF"]
+        .filter(
+          (item) =>
+            item["rdf:Description"]?.["xmlns:rse"] != null ||
+            item["rdf:Description"]?.["xmlns:resonite-ss-ext"] != null,
+        )
+        .map((item) => item["rdf:Description"])
+    : [];
+  if (modDescription.length !== 1) {
+    throw new Error("Invalid XMP format");
+  }
+  const obj = modDescription[0];
+  if (obj["xmlns:rse"] != null) {
+    return parseXML(obj);
+  }
+  if (obj["resonite-ss-ext:PhotoMetadataJson"] != null) {
+    return parseComponentJson(obj["resonite-ss-ext:PhotoMetadataJson"]);
+  }
+
+  throw new Error("Invalid XMP format");
 };
 
 export const loadXMPMetadata = async (file: File) => {
